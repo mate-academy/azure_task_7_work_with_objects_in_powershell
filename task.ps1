43d5c6884b09e3ea8115950cc0681575f0b9d328
Bohdan Mateiko <bohdan.id27182@gmail.com>
2024-04-09 19:19:06 +0300

    test change

--- task.ps1
+++ task.ps1
@@ -1,1 +1,21 @@
-# Write your code here
+$files = Get-ChildItem -Path "$PWD/data"
+
+
+$resultRegions = @()
+foreach ($file in $files) {
+   $region = $file.Name.Replace('.json', '')
+
+
+   $sizes = Get-Content -Path $file.FullName | ConvertFrom-Json
+
+
+   $size = $sizes | Where-Object {$_.Name -eq 'Standard_B2pts_v2'}
+
+
+   if ($size) {
+       $resultRegions += $region
+   }
+}
+
+
+$resultRegions | ConvertTo-Json | Out-File "$PWD/result.json"
